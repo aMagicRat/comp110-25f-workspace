21347859ce3a06bfe4d732705dd33976b563c222
Matthew Moyano <mmoyano@ad.unc.edu>
2025-11-07 22:30:43 -0500

    New changes hopefully this works

--- exercises/ex06/bear.py
+++ exercises/ex06/bear.py
@@ -1,18 +1,17 @@
 """File to define Bear class."""
 
+__author__= """730859678"""
+
 class Bear:
-    
-    
-    def __init__(self):
-        return None
-     self.age= 0 
-     self.hunger_score= 0
 
-    def one_day(self):
+    def __init__(self)->None:
+        self.age = 0
+        self.hunger_score = 0
 
+    def one_day(self)->None:
+        """Simulate one day passing for the bear."""
         self.age += 1
         self.hunger_score -= 1
 
-    def eat(self, num_fish: int):
+    def eat(self, num_fish: int)->None:
         self.hunger_score += num_fish
-      
